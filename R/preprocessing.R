082c437939fc4a47004f3eafa519b8b90a52b320
whopper-cemnl <wjhopper510@gmail.com>
2019-04-18 16:41:12 -0400

    a function for loading data from the csv's for individual subjects into one data frame.
    Also gets rid of unimportant columns, and sets all missing observations to NA  instead of using several inconsistent coding schemes.

---
+++ R/preprocessing.R
@@ -0,0 +1,10 @@
+library(dplyr)
+
+load_data <- function(dir="data"){
+  files <- list.files(dir, pattern = "[0-9]+\\.csv", full.names = TRUE)
+  x <- dplyr::bind_rows(lapply(files, read.csv, na.string = "-", stringsAsFactors = FALSE))
+  x <- select(x, -session, -trial, -colour, -total)
+  no_resp_trials <- x$response == "None"
+  x[no_resp_trials, c("corr", "rt", "response")] <- NA
+  return(x)
+}
