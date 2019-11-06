4fc8a3ebe46ec64109ac6dc4d94136155adca710
whopper-cemnl <wjhopper510@gmail.com>
2019-11-06 13:34:48 -0500

    Update with new fall semseter data

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -7,5 +7,11 @@ load_data <- function(dir="data"){
   no_resp_trials <- x$response == "None"
   x[no_resp_trials, c("corr", "rt", "response")] <- NA
   x$null <- ifelse(x$null == 'yes', TRUE, FALSE)
+
+  x$semester <- ifelse(as.Date(x$date, format = "%Y_%b_%d") < as.Date("2019-09-01"),
+                       "SP19", "FA19"
+                       )
+  x <- select(x, subj, semester, cycle:points)
+  x <- arrange(x, subj, cycle)
   return(x)
 }
